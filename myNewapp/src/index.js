f0cc0ad85952c15f65df313d86cc83a8db40cd27
Aisea Tawake <tawaab1@student.op.ac.nz>
2020-06-08 17:12:13 +1200

    updating package json file

--- myNewapp/src/index.js
+++ myNewapp/src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World from your main file!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+<div>{title}</div>,
+document.getElementById('app')
+);
